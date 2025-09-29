7596f82fbdc4a6e9f209790510d4c53644d0f06b
GURUPRASANNA <guruprasannasuresh@gmail.com>
2025-09-29 11:25:14 -0400

    Feature - Extract frame & audio from Video

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from config import *
 
 
-
 def extract_frames(video_path: str, output_folder: str, interval: int):
     """
     Extracts frames from a video at a fixed interval and saves them to a specified directory.
@@ -14,10 +13,34 @@ def extract_frames(video_path: str, output_folder: str, interval: int):
     Returns:
         None
     """
-    # Your code starts here...
-    pass
-
-
+    # 1. Folder check
+    if not os.path.exists(video_path):
+        raise FileNotFoundError(f"Video not found in the location: {video_path}")
+    
+    os.makedirs(output_folder, exist_ok=True)
+    
+    # 2. Open Video
+    video = cv2.VideoCapture(video_path)
+    
+    # 3. Get Video properties 
+    framespersecond = video.get(cv2.CAP_PROP_FPS)
+    total_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+    # Frame per second = 25, Total frames = 618, Total seconds of the video = 24.72
+    duration_sec = math.floor(total_frames / framespersecond)
+    
+    # 4. Extract frames at fixed given intervals
+    idx = 0
+    for t in range(0, duration_sec + 1, interval):
+        video.set(cv2.CAP_PROP_POS_MSEC, t * 1000)
+        ret, frame = video.read()
+        if not ret or frame is None:
+            continue
+        out_path = os.path.join(output_folder, f"frame_{idx:05d}.jpg")
+        cv2.imwrite(out_path, frame)
+        idx += 1 
+    
+    video.release()
+    
 def extract_audio(video_path: str, output_path: str):
     """
     Extracts the audio track from a video file and saves it as a WAV file.
@@ -29,8 +52,22 @@ def extract_audio(video_path: str, output_path: str):
     Returns:
         None
     """
-    # Your code starts here...
-    pass
+    #1. Folder check
+    if not os.path.exists(video_path):
+        raise FileNotFoundError(f"Video not found: {video_path}")
+    
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    
+    #2. Extract audio using FFMPEG library
+    # It takes the input file path, Output path with some hyperparameters.
+    # acodec - Encoding, ac - Audio channels, ar - Audio sample rate.
+    (
+        ffmpeg
+        .input(video_path)
+        .output(output_path, acodec='pcm_s16le', ac=2, ar='44100')
+        .overwrite_output()
+        .run(quiet=True)
+    )
 
 
 # ------------- DO NOT MODIFY BELOW -------------
